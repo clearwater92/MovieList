a7a572dbb2cac2942b0d74c1493162dae86e9900
clearwater <clearwater92@naver.com>
2021-01-04 22:40:59 +0900

    tdmb api

--- client/src/apis/movies.js
+++ client/src/apis/movies.js
@@ -1,5 +1,13 @@
 import axios from 'axios';
-
+import MovieList from '../components/movies/MovieList';
+import movieReducer from '../reducers/movieReducer';
+export const API_URL = 'https://api.themoviedb.org/3/';
+export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
+export const API_KEY = '6f76d547fab518423df7a041212b23b1';
+// export default axios.create({
+//   baseURL: 'http://localhost:3001',
+// });
+export const URL = `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=ko&page=1&region=KR`;
 export default axios.create({
-  baseURL: 'http://localhost:3001',
-});
+  baseURL: URL
+})
